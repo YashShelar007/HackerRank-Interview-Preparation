37de290318e948b3cff826f4f4e42f6cb454ce69
Yash Shelar <yshelar@asu.edu>
2022-03-17 13:38:10 -0700

    Day 1 Completed

---
+++ Week 1/Day1.js
@@ -0,0 +1,55 @@
+//Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+  // Write your code here
+  let positive = 0,
+    negative = 0,
+    zero = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) positive++;
+    else if (arr[i] < 0) negative++;
+    else zero++;
+  }
+  console.log((positive / arr.length).toFixed(6));
+  console.log((negative / arr.length).toFixed(6));
+  console.log((zero / arr.length).toFixed(6));
+}
+
+function main() {
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  plusMinus(arr);
+}
